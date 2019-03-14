4677c8b23998928110cc54bd39f6f2ce0b1c7f9a
Bernard Correa Pereira <bernard.pereira@dataprev.gov.br>
2019-03-14 13:40:55 -0300

    Correção das Dependencias ( localização)

--- app/app.js
+++ app/app.js
@@ -2,6 +2,6 @@ var React = require('react'); // importa a lib react
 var reactDOM = require('react-dom'); // importa a lib react-dom
 
 reactDOM.render(
-    <h1>Hello World</h1>, 
+    <h1>Hello Raquer</h1>, 
     document.getElementById('app')
 );
