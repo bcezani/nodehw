1a23aea9f71fc67d2fe0b3b1287a835f444ce593
Bernard Correa Pereira <bernard.pereira@dataprev.gov.br>
2019-03-22 11:36:43 -0300

    Importação do json ( mock da lista de tarefas)

--- src/components/todoList/Todo.jsx
+++ src/components/todoList/Todo.jsx
@@ -1,21 +1,36 @@
 import React, { Component } from 'react'
 import TodoTitle from './TodoTitle'
 
-export default class Todo extends Component {
+let mock = require('../../../mock/todo-list/todo-list.json');
 
+
+export default class Todo extends Component {
   
   componentWillMount() {
-    
+    this.setState({ 
+      todolist : mock
+    });
   }
-    
+
 
   render() {
     return (
       <div>
-          <TodoTitle />
+         <TodoTitle />
+         <div>
          Componente de Todo 
          Deverá importar o Todo title ,
          E o todo list
+         </div>
+         { this.state.todolist &&
+          <div>
+            <h1>Dados que devem ser exibidos</h1>
+            <code>
+            {JSON.stringify(this.state.todolist )}
+            </code>
+          
+            </div>
+          }
       </div>
     )
   }
